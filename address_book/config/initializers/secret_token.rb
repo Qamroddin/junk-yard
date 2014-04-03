b0d5222564c5c550991457dffd1292c29b768660
Qamroddin <khamar_md@yahoo.com>
2014-04-03 09:29:08 +0530

    devise

---
+++ address_book/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AddressBook::Application.config.secret_token = '449ad4394c905bdd1fcc44dbccf58ab0697e4accc9daacbbbc7026a01a5d147944a79effa3849d1267a511719b108795aab4521f62da39a70ef8f33ea7779234'
